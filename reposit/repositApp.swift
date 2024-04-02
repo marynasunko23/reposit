1e513931e11e1d5c1d90d47a4b90bac80068b8a9
msunko <maryna.sunko@gmail.com>
2024-04-01 22:40:05 -0400

    Initial Commit

---
+++ reposit/repositApp.swift
@@ -0,0 +1,17 @@
+//
+//  repositApp.swift
+//  reposit
+//
+//  Created by Maryna Sunko on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct repositApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
